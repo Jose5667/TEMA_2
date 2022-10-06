1ab62eca8ae40c2de8767767ebee9e0464a43a5b
Jose <jarios@ceste.com>
2022-10-06 21:53:21 +0200

    Creacion del ejercicio 4 y 5 y parte del 13

--- 06-10-2022_JUEVES/app/src/main/java/com/example/tema_2/MainActivity.java
+++ 06-10-2022_JUEVES/app/src/main/java/com/example/tema_2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tema_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button myboton = (Button) findViewById(R.id.botonborrar);
+        TextView mytexview = (TextView) findViewById(R.id.texview1);
+        myboton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mytexview.setText("");
+            }
+        });
     }
 }
